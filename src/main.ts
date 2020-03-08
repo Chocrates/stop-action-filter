2d9a5f2026c3a8f3a9b6419ffa3188bd4211913d
Chris McIntosh <chocrates@github.com>
2020-03-07 18:51:32 -0600

    Tests now work

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,25 @@
 import * as core from '@actions/core';
 import {GitHub, context} from '@actions/github';
 import {Issue, ActionInputs, CreateInvitationInputs} from './interfaces';
-import {outdent} from 'outdent';
 import * as Octokit from '@octokit/rest';
 import * as pegjs from 'pegjs';
 
-function handleError(error) {
-    core.debug(error.message);
-    core.debug(error.stack || '');
+function handleError(error: any) {
+  core.debug(error.message);
+  core.debug(error.stack || '');
 
-    core.setOutput('message', error.message);
-    core.setOutput('stepStatus', 'failed');
+  core.setOutput('message', error.message);
+  core.setOutput('stepStatus', 'failed');
 
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
 
-function getNextClause({list, current_item = undefined}){
-    var current_idx = current_item ? list.indexOf(current_item) : -1;
-
-    return list[current_idx + 1]
-}
 async function run(): Promise<void> {
-    const filter: string = core.getInput('filter');
-    
-    try {
-        const octokit: GitHub = new GitHub(process.env.GITHUB_TOKEN)
-        const payload: any = context.payload;
+  const filter: string = core.getInput('filter');
 
+  try {
+    const octokit: GitHub = new GitHub(process.env.GITHUB_TOKEN as string);
+    const payload: any = context.payload;
   } catch (error) {
     handleError(error);
   }
